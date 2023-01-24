fea4c08a22eef190cbba37baa3e819f9dc2404d9
程明旺 <1002113844@qq.com>
2023-01-24 18:29:45 +0800

    test

--- bin/index.ts
+++ bin/index.ts
@@ -12,7 +12,6 @@ program
   .option('-b --branch [branchName]', 'diff [branchName] width current branch')
   .description(` lint js or ts by eslint`)
   .action((options) => {
-    console.log(String(options.branch))
     handleES({
       fix: Boolean(options.fix),
       branchName: String(options.branch),

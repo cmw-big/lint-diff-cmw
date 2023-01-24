48826220ef7f69e215c168a4652ab1e01bfb1ac7
程明旺 <1002113844@qq.com>
2023-01-24 18:41:03 +0800

    feat: 修复

--- dist/src/handle/index.js
+++ dist/src/handle/index.js
@@ -4,7 +4,9 @@ exports.handleES = void 0;
 const lintES_1 = require("../lintES");
 const utils_1 = require("../utils");
 function handleES(options) {
-    (0, lintES_1.eslintFileList)((0, utils_1.diffAllFileList)(options?.branchName, ['.ts', '.js', '.tsx', '.jsx']), options);
+    const { branchName } = options ?? {};
+    delete options?.branchName;
+    (0, lintES_1.eslintFileList)((0, utils_1.diffAllFileList)(branchName, ['.ts', '.js', '.tsx', '.jsx']), options);
 }
 exports.handleES = handleES;
 //# sourceMappingURL=index.js.map
